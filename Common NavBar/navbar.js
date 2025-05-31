536e190242afcf8276dc3447aa0c5bf228423e87
Sean Farrell <spfarre@g.clemson.edu>
2025-05-31 17:07:03 -0400

    fixes
    removed redudant code and corrected some documentation

--- Common NavBar/navbar.js
+++ Common NavBar/navbar.js
@@ -1,11 +1,9 @@
-// navbar.js
+// Sean Farrell
+// 5/31/2025
+// CPSC 3750 Wooster
+// ASSIGNMENT: Common NavBar
 document.addEventListener('DOMContentLoaded', () => {
         fetch('navbar.html')
-          .then(response => response.text())
-          .then(data => {
-            document.getElementById('navbar').innerHTML = data;
-          })
-          fetch('./navbar.html')
           .then(response => response.text())
           .then(data => {
             document.getElementById('navbar').innerHTML = data;
